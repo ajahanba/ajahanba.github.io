f7a84f117d794152d0916325660ec4976f8d53cc
ajahanba <afra.mashhadi@gmail.com>
2018-04-12 10:49:51 -0700

    Add files via upload

---
+++ Blog/Blog_files/Blog.js
@@ -0,0 +1,13 @@
+// Created by iWeb 3.0.4 local-build-20170426
+
+setTransparentGifURL('../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("file://localhost/Users/afra/Documents/Website/mashhadi/Blog/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Blog_files/BlogMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');fixupAllIEPNGBGs();fixupIECSS3Opacity('id3');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
